2c6a26d3f7a99406d2b43817c4c6d8edffedd214
Valente Cisneros Gómez <A01736801@tec.mx>
2024-08-26 11:36:28 -0600

    Initial Commit

---
+++ ocoyucan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ocoyucan
+//
+//  Created by Valente Cisneros Gomez on 26/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
