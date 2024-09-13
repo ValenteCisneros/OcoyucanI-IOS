416cda9ee42e0ceafd42afadf47e0f14bf8f3c8f
Valente Cisneros Gómez <A01736801@tec.mx>
2024-09-12 20:40:30 -0600

    Agregado de mapa con map kit, diseño 'colores y botones' y barra de botones abajo

--- ocoyucan/ViewController.swift
+++ ocoyucan/ViewController.swift
@@ -1,19 +1,30 @@
-//
-//  ViewController.swift
-//  ocoyucan
-//
-//  Created by Valente Cisneros Gomez on 26/08/24.
-//
-
 import UIKit
+import SwiftUI
+import MapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Embed the SwiftUI Map inside the UIViewController
+        let mapView = UIHostingController(rootView: ContentView())
+        addChild(mapView)
+        mapView.view.frame = view.bounds
+        view.addSubview(mapView.view)
+        mapView.didMove(toParent: self)
     }
+}
 
+struct ContentView: View {
+    @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        ))
 
+    var body: some View {
+        Map(position: $cameraPosition)
+            .frame(width: 240, height: 323)
+    }
 }
 
