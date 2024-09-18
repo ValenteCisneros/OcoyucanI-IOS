9f63e2f9cb8fadd977ba199e806f0aec16efd8dc
Valente Cisneros Gómez <A01736801@tec.mx>
2024-09-18 11:51:12 -0600

    Detalles del aspecto visual 'orientación, tamaño y color de los botones'  cambio en el tamaño del mapa y Launch Screen

--- ocoyucan/ViewController.swift
+++ ocoyucan/ViewController.swift
@@ -6,25 +6,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Embed the SwiftUI Map inside the UIViewController
-        let mapView = UIHostingController(rootView: ContentView())
-        addChild(mapView)
-        mapView.view.frame = view.bounds
-        view.addSubview(mapView.view)
-        mapView.didMove(toParent: self)
     }
 }
 
 struct ContentView: View {
     @State private var cameraPosition = MapCameraPosition.region(MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            center: CLLocationCoordinate2D(latitude:  18.944816, longitude: -98.330554),
             span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
         ))
-
+     
     var body: some View {
         Map(position: $cameraPosition)
-            .frame(width: 240, height: 323)
+            .frame(width: 358, height: 510)
     }
 }
 
