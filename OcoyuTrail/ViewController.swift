02f7de4f03e0e12dd3863fa70a68f1ce4c46ac0b
Valente Cisneros Gómez <A01736801@tec.mx>
2024-10-30 18:36:50 -0600

    Initial Commit

---
+++ OcoyuTrail/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OcoyuTrail
+//
+//  Created by Valente Cisneros Gomez on 30/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OcoyuTrailTests/OcoyuTrailTests.swift
@@ -0,0 +1,17 @@
+//
+//  OcoyuTrailTests.swift
+//  OcoyuTrailTests
+//
+//  Created by Valente Cisneros Gomez on 30/10/24.
+//
+
+import Testing
+@testable import OcoyuTrail
+
+struct OcoyuTrailTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
